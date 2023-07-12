d1d0bb66e43cff14b62b11b6e97fab7f5bd65722
Piotr Kozlowski <pkozlows@akamai.com>
2023-07-12 17:14:31 +0200

    Adding registerThreadLocalAccessor

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import brave.Tracing;
+import brave.propagation.B3Propagation;
+import brave.sampler.Sampler;
+import io.micrometer.context.ContextRegistry;
 import io.micrometer.tracing.CurrentTraceContext;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.brave.bridge.BraveBaggageManager;
 import io.micrometer.tracing.brave.bridge.BraveCurrentTraceContext;
+import io.micrometer.tracing.brave.bridge.BraveTracer;
+import io.micrometer.tracing.contextpropagation.ObservationAwareSpanThreadLocalAccessor;
+import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
@@ -29,5 +38,26 @@ public class DemoApplication {
 	WebClient webClient(WebClient.Builder builder) {
 		return builder.build();
 	}
+
+    @Bean
+    public Tracing braveTracing(brave.propagation.CurrentTraceContext currentTraceContext) {
+        return Tracing.newBuilder()
+                .currentTraceContext(currentTraceContext)
+                .supportsJoin(false)
+                .traceId128Bit(true)
+                .propagationFactory(B3Propagation.newFactoryBuilder()
+                        .injectFormat(B3Propagation.Format.MULTI)
+                        .build())
+                .sampler(Sampler.NEVER_SAMPLE)
+                .build();
+    }
+
+    // @Bean
+    // public Tracer tracer(CurrentTraceContext currentTraceContext, Tracing braveTracing) {
+    //     brave.Tracer tracer = braveTracing.tracer();
+    //     BraveTracer braveTracer = new BraveTracer(tracer, currentTraceContext, new BraveBaggageManager());
+    //     ContextRegistry.getInstance().registerThreadLocalAccessor(new ObservationAwareSpanThreadLocalAccessor(braveTracer));
+    //     return braveTracer;
+    // }
 
 }
--- src/main/java/com/example/demo/ExampleController.java
+++ src/main/java/com/example/demo/ExampleController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.micrometer.context.ContextRegistry;
 import io.micrometer.tracing.CurrentTraceContext;
+import io.micrometer.tracing.TraceContext;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.contextpropagation.ObservationAwareSpanThreadLocalAccessor;
+import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
 
 @RestController
 @RequestMapping("/tracing")
@@ -17,6 +23,13 @@ public class ExampleController {
 	CurrentTraceContext context;
 	@Autowired
 	WebClient webClient;
+	@Autowired
+	Tracer tracer;
+
+	@PostConstruct
+	void setup() {
+		ContextRegistry.getInstance().registerThreadLocalAccessor(new ObservationAwareSpanThreadLocalAccessor(tracer));
+	}
 
 	@GetMapping("/webclient")
 	public Mono<Void> getWebclient() {
@@ -26,7 +39,7 @@ public class ExampleController {
 						.retrieve()
 						.bodyToMono(String.class)
 						.doOnNext(next -> {
-/*context is null here ->*/ System.out.println(context.context());
+							/* context is null here -> */ System.out.println(context.context());
 						})
 						.subscribe())
 				.then();
